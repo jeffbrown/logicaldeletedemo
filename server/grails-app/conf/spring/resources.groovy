b4d130b67969c560e56cb9083d7068198ba794b1
Jeff Scott Brown <brownj@objectcomputing.com>
2017-12-15 08:50:12 -0500

    No longer needed
    See
    https://github.com/grails-plugins/gorm-logical-delete/commit/ba280f20362
    998fc9d06bbc2155982fa52a0cf4e

--- server/grails-app/conf/spring/resources.groovy
+++ server/grails-app/conf/spring/resources.groovy
@@ -1,10 +1,4 @@
-import gorm.logical.delete.PreQueryListener
-
 // Place your Spring DSL code here
 beans = {
 
-    // this won't be necessary once the
-    // plugin starts registering this listener
-    // automatically...
-    preQueryListener PreQueryListener
 }
